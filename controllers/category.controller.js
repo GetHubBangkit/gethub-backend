876ae3fa150e41eb4de9131ed6da283ba5270c45
AmurTigro <martiohusein27@gmail.com>
2024-06-02 11:40:20 +0700

    Created CRUD functionality for category

---
+++ controllers/category.controller.js
@@ -0,0 +1,151 @@
+const { check } = require("express-validator");
+const models = require("../models");
+
+const getAllCategories = async (req, res, next) => {
+  try {
+    const categories = await models.Category.findAll();
+    if (!categories || categories.length === 0) {
+      return res.status(404).json({
+        success: false,
+        data: [],
+        message: 'Kategori tidak ditemukan',
+        error_code: 404,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: categories,
+      message: "Kategori berhasil diambil",
+      error_code: 0,
+    });
+  } catch (error) {
+    console.error("Error retrieving categories:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+const getCategoryById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const category = await models.Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        data: {},
+        message: 'Kategori tidak ditemukan',
+        error_code: 404,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: category,
+      message: "Kategori berhasil diambil",
+      error_code: 0,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+const createCategory = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const customName = name.toLowerCase();
+    const checkCategory = await models.Category.findOne({ where: { name: customName } });
+
+    if (checkCategory) {
+      return res.status(409).json({
+        success: false,
+        message: 'Kategori sudah ada',
+        error_code: 409,
+      });
+    }
+    const newCategory = await models.Category.create({ name: customName });
+    res.status(201).json({
+      success: true,
+      data: newCategory,
+      message: "Kategori berhasil dibuat",
+      error_code: 0,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+const updateCategory = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { name } = req.body;
+    const customName = name.toLowerCase();
+    let category = await models.Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: 'Kategori tidak ditemukan',
+        error_code: 404,
+      });
+    }
+    await models.Category.update(
+      { name: customName },
+      { where: { id: id } }
+    );
+    category = await models.Category.findByPk(id);
+    res.status(200).json({
+      success: true,
+      message: "Kategori berhasil diperbarui",
+      error_code: 0,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+const deleteCategory = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const category = await models.Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: 'Kategori tidak ditemukan',
+        error_code: 404,
+      });
+    }
+    await models.Category.destroy({ where: { id: id } });
+    res.status(204).json({
+      success: true,
+      message: "Kategori berhasil dihapus",
+      error_code: 0,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+module.exports = {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
---
+++ models/category.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Category has many product, project, and certification
+      this.hasMany(models.Product, { foreignKey: 'category_id' });
+      this.hasMany(models.Project, { as: 'category', foreignKey: 'category_id' });
+      this.hasMany(models.Certification, { foreignKey: 'category_id' });
+    }
+  }
+  Category.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Category',
+    tableName: 'categories',
+    validateUpdateColumns() {
+      const allowedColumns = ["name"];
+      for (const key in this._changed) {
+        if (!allowedColumns.includes(key)) {
+          throw new Error(`Kolom ${key} tidak dapat diperbarui`);
+        }
+      }
+    }
+  });
+
+  return Category;
+};
