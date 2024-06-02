367bf236bc302fe33456d9ac6174b8dd79475783
AmurTigro <martiohusein27@gmail.com>
2024-06-02 11:39:38 +0700

    Created schema for user input validation

--- middleware/input-validator.js
+++ middleware/input-validator.js
@@ -1,9 +1,13 @@
-
 const {
-  userRegisterSchema,
-  userLoginSchema,
-} = require('../schemas')
+  userRegisterSchema, userLoginSchema,
+  productSchema, linkSchema, sponsorSchema,
+  informationSchema, partnerSchema,
+  userUpdateSchema, certificationSchema,
+  projectSchema, categorySchema, projectReviewFreelanceSchema,
+  projectTaskSchema, projectReviewSchema, projectUserBidSchema
+} = require("../schemas");
 
+//* User
 exports.validateRegisterUser = (req, res, next) => {
   const { error } = userRegisterSchema.validate(req.body);
   if (error) {
@@ -57,6 +61,466 @@ exports.validateLoginUser = (req, res, next) => {
       message: messages,
       error_code: 400,
     })
+  } else {
+    next();
+  }
+};
+
+exports.validateUpdateUser = (req, res, next) => {
+  const { error } = userUpdateSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "full_name":
+            return "Nama lengkap tidak boleh kosong.";
+          case "profession":
+            return "Profesi tidak boleh kosong.";
+          case "phone":
+            return "Nomor telepon harus terdiri dari 10 hingga 15 angka.";
+          case "email":
+            return "Email tidak valid.";
+          case "address":
+            return "Alamat lengkap tidak boleh kosong.";
+          case "theme_hub":
+            return "Theme_hub tidak boleh kosong."
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    })
+  } else {
+    next()
+  }
+}
+//* Product
+
+exports.validateProduct = (req, res, next) => {
+  const { error } = productSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case 'name':
+            return 'Nama produk tidak boleh kosong.';
+          case 'description':
+            return 'Deskripsi tidak boleh kosong.';
+          case 'category_id':
+            return 'Id kategori tidak boleh kosong.';
+          case 'image_url':
+            return 'URL gambar tidak valid.';
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+
+//* Link
+exports.validateLink = (req, res, next) => {
+  const { error } = linkSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case 'category':
+            return 'Kategori tidak boleh kosong.';
+          case 'link':
+            return 'Link tidak valid.';
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+
+//* History Upload
+
+exports.validateHistoryUpload = (req, res, next) => {
+  const { error } = historyUploadSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case 'link':
+            return 'Link tidak valid.';
+          case 'extension':
+            return 'Ekstensi harus berupa string.';
+          default:
+            return el.message.replace(/"/g, '');
+        }
+      })
+      .join(', ');
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+//* Sponsor
+
+exports.validateSponsor = (req, res, next) => {
+  const { error } = sponsorSchema.validate(req.body);
+
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case 'name':
+            return 'Nama sponsor tidak boleh kosong.';
+          case 'image_url':
+            return 'URL gambar tidak valid.';
+          case 'link':
+            return 'Link sponsor tidak valid.';
+          default:
+            return el.message.replace(/"/g, '');
+        }
+      })
+      .join(', ');
+
+    return res.status(420).json({
+      success: false,
+      message: messages,
+      error_code: 420,
+    })
+  } else {
+    next();
+  }
+}
+
+//* Information
+
+exports.validateInformation = (req, res, next) => {
+  const { error } = informationSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case 'title':
+            return 'Judul informasi tidak boleh kosong.';
+          case 'description':
+            return 'Deskripsi informasi tidak boleh kosong.';
+          case 'image_url':
+            return 'URL gambar tidak valid.';
+          case 'category':
+            return "Category tidak boleh kosong";
+          case 'is_active':
+            return "Is_active tidak boleh kosong";
+          default:
+            return el.message.replace(/"/g, '');
+        }
+      })
+      .join(', ');
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400
+    })
+  } else {
+    next();
+  }
+}
+
+//* Partner
+
+exports.validatePartner = (req, res, next) => {
+  const { error } = partnerSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "email":
+            return "Email tidak valid.";
+          case "password":
+            return "Password harus terdiri dari minimal 6 karakter.";
+          case "full_name":
+            return "Nama lengkap harus diisi.";
+          case "profession":
+            return "Profesi harus diisi.";
+          case "phone":
+            return "Nomor telepon harus diisi.";
+          case "photo":
+            return "Foto harus diisi.";
+          case "address":
+            return "Alamat harus diisi.";
+          case "website":
+            return "Website tidak valid.";
+          case "image":
+            return "Image tidak valid.";
+          default:
+            return el.message.replace(/"/g, '');
+        }
+      })
+      .join(', ');
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400
+    })
+  } else {
+    next();
+  }
+}
+
+//* Certification
+
+exports.validateCertification = (req, res, next) => {
+  const { error } = certificationSchema.validate(req.body);
+
+  if (error) {
+    const messages = error.details.map((el) => {
+      switch (el.context.key) {
+        case "title":
+          return "Title harus diisi.";
+        case "category_id":
+          return "Category harus diisi.";
+        case "image":
+          return "Image harus diisi.";
+        default:
+          return el.message.replace(/"/g, '');
+      }
+    }).join(', ')
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400
+    })
+  }
+}
+
+//* Category
+
+exports.validateCategory = (req, res, next) => {
+  const { error } = categorySchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "name":
+            return "Nama kategori harus diisi.";
+          default:
+            return el.message.replace(/"/g, '');
+        }
+      }).join(', ');
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400
+    })
+  }
+}
+
+
+
+//* Project
+
+exports.validateProject = (req, res, next) => {
+  const { error } = projectSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "owner_id":
+            return "ID Pemilik diperlukan dan harus berupa UUID yang valid.";
+          case "title":
+            return "Judul diperlukan.";
+          case "category_id":
+            return "ID Kategori diperlukan dan harus berupa UUID yang valid.";
+          case "min_budget":
+          case "max_budget":
+            return "Anggaran harus berupa angka.";
+          case "min_deadline":
+          case "max_deadline":
+            return "Batas waktu harus berupa tanggal yang valid dalam format ISO.";
+          case "chatroom_id":
+            return "ID Ruang Obrolan diperlukan dan harus berupa UUID yang valid.";
+          case "status_project":
+            return "Status proyek harus salah satu dari: BUKA, PENAWARAN, TUTUP, SELESAI.";
+          case "status_freelance_task":
+            return "Status tugas freelancer harus salah satu dari: BUKA, TUTUP.";
+          case "status_payment":
+            return "Status pembayaran harus salah satu dari: MENUNGGU, PENYELESAIAN.";
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+//* Project User Bid
+exports.validateProjectUserBid = (req, res, next) => {
+  const { error } = projectUserBidSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "project_id":
+            return "ID Proyek diperlukan dan harus berupa UUID yang valid.";
+          case "user_id":
+            return "ID Pengguna diperlukan dan harus berupa UUID yang valid.";
+          case "budget_bid":
+            return "Penawaran anggaran diperlukan dan harus berupa angka.";
+          case "is_selected":
+            return "Status seleksi harus berupa boolean.";
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+
+//* Project Task
+
+exports.validateProjectTask = (req, res, next) => {
+  const { error } = projectTaskSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "project_id":
+            return "ID Proyek diperlukan dan harus berupa UUID yang valid.";
+          case "task_number":
+            return "Nomor tugas diperlukan dan harus berupa bilangan bulat.";
+          case "task_description":
+            return "Deskripsi tugas diperlukan.";
+          case "task_status":
+            return "Status tugas harus salah satu dari: DALAM PROSES, ULANGI, SELESAI.";
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+//* Project Review
+
+
+exports.validateProjectReview = (req, res, next) => {
+  const { error } = projectReviewSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "project_id":
+            return "ID Proyek diperlukan dan harus berupa UUID yang valid.";
+          case "owner_id":
+            return "ID Pemilik diperlukan dan harus berupa UUID yang valid.";
+          case "freelance_id":
+            return "ID Freelancer diperlukan dan harus berupa UUID yang valid.";
+          case "message":
+            return "Pesan diperlukan.";
+          case "sentiment":
+            return "Sentimen diperlukan.";
+          case "sentiment_score":
+            return "Skor sentimen diperlukan dan harus berupa angka.";
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+//* Project Review Freelance
+
+exports.validateProjectReviewFreelance = (req, res, next) => {
+  const { error } = projectReviewFreelanceSchema.validate(req.body);
+  if (error) {
+    const messages = error.details
+      .map((el) => {
+        switch (el.context.key) {
+          case "project_id":
+            return "ID Proyek diperlukan dan harus berupa UUID yang valid.";
+          case "owner_id":
+            return "ID Pemilik diperlukan dan harus berupa UUID yang valid.";
+          case "freelance_id":
+            return "ID Freelancer diperlukan dan harus berupa UUID yang valid.";
+          case "message":
+            return "Pesan diperlukan.";
+          case "sentiment":
+            return "Sentimen diperlukan.";
+          case "sentiment_score":
+            return "Skor sentimen diperlukan dan harus berupa angka.";
+          default:
+            return el.message.replace(/"/g, "");
+        }
+      })
+      .join(", ");
+
+    return res.status(400).json({
+      success: false,
+      message: messages,
+      error_code: 400,
+    });
   } else {
     next();
   }
--- schemas.js
+++ schemas.js
@@ -44,6 +44,8 @@ exports.userRegisterSchema = Joi.object({
   is_verify: Joi.boolean().optional(),
   is_complete_profile: Joi.boolean().optional(),
   is_premium: Joi.boolean().optional(),
+  is_verif_ktp: Joi.boolean().optional(),
+  is_verif_ktp_url: Joi.string().allow("").optional(),
   theme_hub: Joi.number().integer().optional(),
   role_id: Joi.string().guid({ version: "uuidv4" }).optional(),
   sentiment_owner_analisis: Joi.string().allow("").optional(),
@@ -70,6 +72,176 @@ exports.userLoginSchema = Joi.object({
   qr_code: Joi.string().allow("").optional(),
   is_verify: Joi.boolean().optional(),
   is_complete_profile: Joi.boolean().optional(),
+  is_verif_ktp: Joi.boolean().optional(),
+  is_verif_ktp_url: Joi.string().allow("").optional(),
   is_premium: Joi.boolean().optional(),
   theme_hub: Joi.number().integer().optional(),
 });
+
+//* Complete Profile
+exports.userUpdateSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  full_name: Joi.string().optional(),
+  profession: Joi.string().allow("").required(),
+  phone: Joi.string()
+    .allow("")
+    .pattern(/^\d{10,15}$/)
+    .required(),
+  email: Joi.string().email().optional(),
+  web: Joi.string().uri().optional(),
+  address: Joi.string().allow("").required(),
+  photo: Joi.string().allow("").required(),
+  about: Joi.string().allow("").optional(),
+  theme_hub: Joi.number().integer().optional(),
+})
+
+//*Product
+
+exports.productSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  user_id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  category_id: Joi.string().required(),
+  image_url: Joi.string().uri().required(),
+
+});
+
+//* Link
+
+exports.linkSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  user_id: Joi.string().guid({ version: "uuidv4" }).allow(null).optional(),
+  category: Joi.string().required(),
+  link: Joi.string().uri().required(),
+
+});
+
+//* History Upload
+
+exports.historyUploadSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  user_id: Joi.string().guid({ version: "uuidv4" }).required(),
+  link: Joi.string().optional(),
+  extension: Joi.string().optional(),
+});
+
+//* Sponsor
+
+exports.sponsorSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  name: Joi.string().required(),
+  image_url: Joi.string().uri().required(),
+  link: Joi.string().uri().required(),
+  is_active: Joi.boolean().required(),
+})
+
+//* Information
+exports.informationSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  image_url: Joi.string().required(),
+  category: Joi.string().required(),
+  is_active: Joi.boolean().required()
+});
+
+//* Partner
+
+exports.partnerSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  user_id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  ref_user_id: Joi.string().optional(),
+  full_name: Joi.string().optional(),
+  qr_code: Joi.string().optional(),
+  profession: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string().optional(),
+  photo: Joi.string().optional(),
+  address: Joi.string().optional(),
+  website: Joi.string().uri().optional(),
+  image: Joi.string().optional(),
+});
+
+//* Certification
+
+exports.certificationSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  title: Joi.string().required(),
+  category_id: Joi.string().required(),
+  image: Joi.string().required(),
+})
+
+//* Category
+
+exports.categorySchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  name: Joi.string().required(),
+})
+
+//* Project
+
+exports.projectSchema = Joi.object({
+  id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  owner_id: Joi.string().guid({ version: "uuidv4" }).optional(),
+  title: Joi.string().required(),
+  category_id: Joi.string().guid({ version: "uuidv4" }).required(),
+  description: Joi.string().allow(null).optional(),
+  min_budget: Joi.number().required(),
+  max_budget: Joi.number().required(),
+  min_deadline: Joi.date().required(),
+  max_deadline: Joi.date().required(),
+  created_date: Joi.date().allow(null).optional(),
+  chatroom_id: Joi.string().optional(),
+  is_active: Joi.boolean().optional(),
+  banned_message: Joi.string().allow(null).optional(),
+  status_project: Joi.string().valid('OPEN', 'BID', 'CLOSE', 'FINISHED').optional(),
+  status_freelance_task: Joi.string().valid('OPEN', 'CLOSE').optional(),
+  status_payment: Joi.string().valid('WAITING', 'SETTLEMENT').optional(),
+  fee_owner_transaction_persen: Joi.number().allow(null).optional(),
+  fee_owner_transaction_value: Joi.number().allow(null).optional(),
+  fee_freelance_transaction_persen: Joi.number().allow(null).optional(),
+  fee_freelance_transaction_value: Joi.number().allow(null).optional(),
+});
+
+//* Project Task
+exports.projectTaskSchema = Joi.object({
+  id: Joi.string().uuid({ version: 'uuidv4' }).optional(),
+  project_id: Joi.string().uuid({ version: 'uuidv4' }).optional(),
+  task_number: Joi.number().integer().required(),
+  task_description: Joi.string().required(),
+  task_status: Joi.string().valid('ON-PROGRESS', 'REVIEW', 'REVISION', 'DONE').optional(),
+  task_feedback: Joi.string().allow('').optional(),
+}).options({ stripUnknown: true });
+
+//* Project User Bid
+
+exports.projectUserBidSchema = Joi.object({
+  id: Joi.string().uuid({ version: 'uuidv4' }).optional(),
+  project_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  budget_bid: Joi.number().required(),
+  message: Joi.string().allow('').optional(),
+}).options({ stripUnknown: true });
+
+//* Project Review
+exports.projectReviewSchema = Joi.object({
+  id: Joi.string().uuid({ version: 'uuidv4' }).optional(),
+  project_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  owner_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  freelance_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  message: Joi.string().required(),
+  sentiment: Joi.string().required(),
+  sentiment_score: Joi.number().required(),
+}).options({ stripUnknown: true });
+
+//* Project Review Freelance 
+
+exports.projectReviewFreelanceSchema = Joi.object({
+  id: Joi.string().uuid({ version: 'uuidv4' }).optional(),
+  project_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  owner_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  freelance_id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+  message: Joi.string().required(),
+  sentiment: Joi.string().required(),
+  sentiment_score: Joi.number().required(),
+}).options({ stripUnknown: true });
