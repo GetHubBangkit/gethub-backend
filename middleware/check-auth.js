df28e863d2e8925f59692caf8d82cbd7579c5a9e
AmurTigro <martiohusein27@gmail.com>
2024-06-02 11:43:07 +0700

    Created another authentication middleware

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,6 +1,7 @@
-const { verifyAccessToken } = require("../helpers/utility");
+const { verifyAccessToken, getUserId } = require("../helpers/utility");
+const models = require('../models')
 
-exports.authenticateToken = async (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
@@ -25,4 +26,81 @@ exports.authenticateToken = async (req, res, next) => {
 
   req.data = result.token;
   next();
+};
+
+
+const checkPortfolio = async (req, res, next) => {
+  try {
+
+    const {user_id} = getUserId(req);
+    const project_id = req.body.project_id;
+    const project = await models.Project.findByPk(project_id);
+
+    const project_category_id = project.category_id;
+
+    const hasMatchingProduct = await models.Product.findOne({
+      where: {
+        user_id,
+        category_id: project_category_id
+      }
+    });
+
+    const hasMatchingCertification = await models.Certification.findOne({
+      where: {
+        user_id,
+        category_id: project_category_id
+      }
+    });
+
+    if (!hasMatchingProduct && !hasMatchingCertification) {
+      return res.status(405).json({
+        success: false,
+        message: "Anda tidak memiliki portofolio yang sesuai dengan kategori proyek ini",
+        error_code: 405,
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Kesalahan dalam pemeriksaan portofolio:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Kesalahan internal server",
+      error_code: 500,
+    });
+  }
+};
+
+module.exports = checkPortfolio;
+
+
+const verifyUserMiddleware = async (req, res, next) => {
+  try {
+    const { user_id } = getUserId(req);
+    const user = await models.User.findByPk(user_id);
+    
+    if (!user || !user.is_verify || !user.is_complete_profile) {
+      return res.status(403).json({
+        success: false,
+        message: "Pengguna belum diverifikasi atau profil pengguna belum lengkap",
+        error_code: 403,
+      });
+    }
+    
+    next();
+  } catch (error) {
+    console.error("Terjadi kesalahan saat memverifikasi pengguna:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Gagal memverifikasi pengguna",
+      error_code: 500,
+    });
+  }
+};
+
+
+module.exports = {
+  authenticateToken,
+  verifyUserMiddleware,
+  checkPortfolio
 };
