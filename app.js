ba699b57c0af40544952b24e940b70531fab5d24
AmurTigro <martiohusein27@gmail.com>
2024-06-02 11:42:19 +0700

    Added the route to our main app.js

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require('cors');
 
+const { authenticateToken } = require("./middleware/check-auth");
+const { upload, imageUploader } = require("./helpers/image-uploader");
+
+
 const userRoute = require('./routes/user');
+const productRoute = require("./routes/product");
+const linkRoute = require("./routes/link");
+const certificateRoute = require("./routes/certification");
+const categoryRoute = require("./routes/category")
 
 const app = express();
 
@@ -16,5 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", userRoute);
+app.use("/api", productRoute);
+app.use("/api", linkRoute);
+app.use("/api", certificateRoute);
+app.use("/api", categoryRoute);
+
+
+//* Helpers
+app.post("/api/upload-file", authenticateToken, upload.single("file"), imageUploader);
+
 
 module.exports = app;
