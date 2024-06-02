72be998808fba0d5c31ae380ae644fd9c857aac0
AmurTigro <martiohusein27@gmail.com>
2024-06-02 12:01:19 +0700

    Updated the main app.js to the newest changes

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ const { authenticateToken } = require("./middleware/check-auth");
 const { upload, imageUploader } = require("./helpers/image-uploader");
 
 
-const userRoute = require('./routes/user');
+const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const linkRoute = require("./routes/link");
+const enumeRoute = require("./routes/enumeration");
+const sponsorRoute = require("./routes/sponsor");
+const partnerRoute = require("./routes/partner");
+const informationRoute = require("./routes/information");
 const certificateRoute = require("./routes/certification");
 const categoryRoute = require("./routes/category")
+
 
 const app = express();
 
@@ -28,6 +33,10 @@ app.use("/api", productRoute);
 app.use("/api", linkRoute);
 app.use("/api", certificateRoute);
 app.use("/api", categoryRoute);
+app.use("/api", enumeRoute);
+app.use("/api", sponsorRoute);
+app.use("/api", partnerRoute);
+app.use("/api", informationRoute);
 
 
 //* Helpers
