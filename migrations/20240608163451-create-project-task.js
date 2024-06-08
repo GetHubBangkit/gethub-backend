78100251fc9e162bf03b3fd4ec5382e335661afa
AmurTigro <martiohusein27@gmail.com>
2024-06-08 23:47:39 +0700

    Create project task model and migration

---
+++ migrations/20240608163451-create-project-task.js
@@ -0,0 +1,62 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('project_tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      project_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'projects',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      freelancer_id: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      task_number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      task_description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      task_status: {
+        type: Sequelize.ENUM('ON-PROGRESS','REVIEW', 'REVISION', 'DONE'),
+        allowNull: false,
+        defaultValue: 'ON-PROGRESS'
+      },
+      task_feedback: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('project_tasks');
+  }
+};
---
+++ models/project_task.js
@@ -0,0 +1,68 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Project_Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Each task belongs to one project
+      Project_Task.belongsTo(models.Project, { as:'project_tasks', foreignKey: 'project_id' });
+      // Each task can be assigned to one freelance user
+      Project_Task.belongsTo(models.User, { as: 'freelancer', foreignKey: 'freelancer_id' });
+    }
+  }
+  Project_Task.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    project_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'Projects',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    freelancer_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    task_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    task_description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    task_status: {
+      type: DataTypes.ENUM('ON-PROGRESS', 'REVIEW', 'REVISION', 'DONE'),
+      allowNull: false,
+      defaultValue: 'ON-PROGRESS'
+    },
+    task_feedback: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }   
+  }, {
+    sequelize,
+    modelName: 'Project_Task',
+    tableName: 'project_tasks',
+  });
+  return Project_Task;
+};
+
