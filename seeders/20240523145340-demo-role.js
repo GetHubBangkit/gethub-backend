64377dcf4bee3d2c5c7a4f20ce9e17c06f79fe01
ahmdirvn <ahmadirvan9c17@gmail.com>
2024-06-18 09:23:18 +0700

    Add seeders for roles, banks, web viewers, and categories

---
+++ seeders/20240523145340-demo-role.js
@@ -0,0 +1,25 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('roles', [
+      {
+        id: uuidv4(),
+        name: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: uuidv4(),
+        name: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('roles', { name: ['user', 'admin'] }, {});
+  }
+};
